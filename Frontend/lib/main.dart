9cd8f956d8428358ae68880b9c7d977151069d8c
Pemax4321 <senithu.20220888@iit.ac.lk>
2025-02-12 18:56:10 +0530

    authentication part

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'notifications_page.dart';
 import 'vr_sessions_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
-}
+  }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
