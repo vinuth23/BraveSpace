cacb1d10c115c96108ac25a1a274b5382ad95ee1
Yasara22 <yasarathilothma73@gmail.com>
2025-03-10 23:20:55 +0530

    Update avatar_customization_page.dart

--- Frontend/lib/avatar_customization_page.dart
+++ Frontend/lib/avatar_customization_page.dart
@@ -16,3 +16,28 @@ class AvatarApp extends StatelessWidget {
     );
   }
 }
+
+class AvatarCustomizationScreen extends StatefulWidget {
+  @override
+  _AvatarCustomizationScreenState createState() => _AvatarCustomizationScreenState();
+}
+
+class _AvatarCustomizationScreenState extends State<AvatarCustomizationScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  Color selectedSkinTone = Colors.bisque;
+  String selectedHair = "assets/hair1.png";
+  String selectedOutfit = "assets/outfit1.png";
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+    _loadAvatarPreferences();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
